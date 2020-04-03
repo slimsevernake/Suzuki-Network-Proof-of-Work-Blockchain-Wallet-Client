0323e6a7cf78ba39401d9438058f31c56cb161cd
Goldmund <matthewtzito@gmail.com>
2020-04-02 18:28:03 -0700

    init primary blockchain dir

--- mini_chain.py
+++ mini_chain.py
@@ -1,17 +1,31 @@
 def lightning_hash(data):
     return data + "*"
 
-
-
 class Block:
-  def __init__(self, data, hash, last_hash):
+  def __init__(self, data, hash, prev_hash):
     self.data = data
     self.hash = hash
-    self.last_hash = last_hash 
+    self.prev_hash = prev_hash 
 
 
-foo_block = Block('foodata','foohash','lastfoohash')
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        genesis = Block('gen_data', 'gen_hash', 'gen_prev_hash')
+        
+        self.chain = [genesis]
 
-print(foo_block.data)
-print(foo_block.hash)
-print(foo_block.last_hash)
+    def add_block(self, data):
+        prev_hash = self.chain[-1].hash
+        hash = lightning_hash(data + prev_hash)
+        block = Block(data, hash, prev_hash)
+
+        self.chain.append(block)
+
+foo_blockchain = Blockchain()
+foo_blockchain.add_block('one')
+foo_blockchain.add_block('two')
+foo_blockchain.add_block('three')
+
+for block in foo_blockchain.chain:
+    print(block.__dict__)
