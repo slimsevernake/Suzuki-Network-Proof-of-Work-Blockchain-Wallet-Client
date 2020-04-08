bae1780aab9e88fb6d028af5980d618cb6c2f946
Goldmund <matthewtzito@gmail.com>
2020-04-08 16:25:09 -0700

    finish base frontend functionality

--- primary-blockchain/frontend/src/components/Pool.jsx
+++ primary-blockchain/frontend/src/components/Pool.jsx
@@ -1,16 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Transaction from "./Transaction.jsx";
-import { ROOT_PATH } from "../config.js";
+import { ROOT_PATH, SECONDS_JS } from "../config.js";
+import history from "../history.js";
+
+const POLL_INTERVAL = 10 * SECONDS_JS;
 
 const Pool = () => {
     const [tx, setTx] = useState([]);
-
-    useEffect(() => {
+    const fetchTx = () => {
         fetch(`${ROOT_PATH}/transactions`)
             .then(res => res.json())
             .then(json => setTx(json));
+    }
+
+    useEffect(() => {
+        fetchTx();
+        // poll to keep pool updated, clear 
+        const poll = setInterval(fetchTx, POLL_INTERVAL);
+        // clear when unmount so interval isn't trying to set unmounted state
+        return () => clearInterval(poll)
     }, []);
+
+    const mineBlock = () => {
+        fetch(`${ROOT_PATH}/blockchain/mine`)
+            .then(() => {
+                alert("Success.");
+
+                history.push("/blockchain");
+            })
+    }
 
     return (
         <div className="TransactionPool">
@@ -25,6 +44,10 @@ const Pool = () => {
                     </div>
                 ))}
             </div>
+            <hr />
+            <button onClick={mineBlock}>
+                Mine a block.
+            </button>
         </div>
     )
 }
